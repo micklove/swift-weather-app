c8a7bb9b22f71373e1b232f17cabbec14fc043b0
micklove <micklove+github@gmail.com>
2016-06-11 18:02:04 +1000

    Initial Commit

---
+++ mlo.weather.appTests/mlo_weather_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  mlo_weather_appTests.swift
+//  mlo.weather.appTests
+//
+//  Created by Love Michael on 11/06/2016.
+//  Copyright © 2016 Love Michael. All rights reserved.
+//
+
+import XCTest
+@testable import mlo_weather_app
+
+class mlo_weather_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
